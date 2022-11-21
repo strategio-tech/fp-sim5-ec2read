d3a878056ce5f45b1ad515057179f9ecad798070
JosephCoiffman <jcoiffman@gmail.com>
2022-11-21 09:53:52 -0500

    fixed test

--- testing.py
+++ testing.py
@@ -1,9 +1,9 @@
 import pytest
 from app import app
 
-client = app.test_client()
+clients = app.test_client()
 
-def test_home(client):
-    resp = client.get('/login')
+def test_home():
+    resp = clients.get('/login')
     assert resp.status_code == 200
 
