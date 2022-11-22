b1b09046afad063403ff2f938f8d8a6d01a23079
JosephCoiffman <67018154+JosephCoiffman@users.noreply.github.com>
2022-11-22 12:37:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -140,4 +140,4 @@ def send_content(path):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, threaded=True, host='0.0.0.0', port=5000)
